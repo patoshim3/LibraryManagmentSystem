803fe7b9aca871cc5c11ada21b3d992a649f3860
patoshim3 <patimaactposhta@gmail.com>
2025-12-23 22:56:15 +0500

    feature entity author first init and adding id, name, authorBiography

---
+++ src/main/java/com/example/librarymanagmentsystem/Model/Author.java
@@ -0,0 +1,25 @@
+package com.example.librarymanagmentsystem.Model;
+
+import jakarta.persistence.*;
+import lombok.AllArgsConstructor;
+import lombok.Getter;
+import lombok.NoArgsConstructor;
+import lombok.Setter;
+
+@Entity
+@Getter
+@Setter
+@AllArgsConstructor
+@NoArgsConstructor
+@Table(name = "authors")
+public class Author {
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    private Long id;
+
+    @Column(nullable = false)
+    private String name;
+
+    @Column(name = "biography")
+    private String authorBiography;
+}
