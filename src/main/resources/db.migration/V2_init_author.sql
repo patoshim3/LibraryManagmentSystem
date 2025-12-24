a26632cdb7bb7e187fcb434c68f9711b4aba2aba
patoshim3 <patimaactposhta@gmail.com>
2025-12-24 15:40:11 +0500

    migration test flyway V1_init.sql for init author and book, V2_init_author.sql for insert author, V3_init_book.sql for insert book

---
+++ src/main/resources/db.migration/V2_init_author.sql
@@ -0,0 +1,3 @@
+insert into authors (name, authorBiography) VALUES ('Abay', 'about AbayauthorBiography');
+insert into authors (authorBiography, name) VALUES ('about Mukhtar authorBiography', 'Muktar');
+insert into authors (authorBiography, name) VALUES ('about Shokan authorBiography', 'Shokan');
